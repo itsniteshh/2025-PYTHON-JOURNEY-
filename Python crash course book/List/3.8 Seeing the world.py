b762563a618502f29c368a8cdc710839d330edb4
Nitesh <nitesh7258@gmail.com>
2025-02-24 01:28:14 +0530

    Day 1

--- Python crash course book/List/3.8 Seeing the world.py
+++
@@ -1,8 +0,0 @@
-places_to_visit = ["J&k", "Himachal", "Nepal", "Abu dhabi", "Kerala"]
-print("Original order: ", places_to_visit, "\n")
-    
-print(sorted(places_to_visit), "\n")
-
-print("Original order: ", places_to_visit)
-
-print("\n", sorted(places_to_visit[::-1]), "\n")
