cf31ab7f7f09f863e68bcd050f2875e8edc5356d
Nitesh <nitesh7258@gmail.com>
2025-03-10 17:38:12 +0530

    Day 1

---
+++ Python crash course book/if statements/5.3 alien colors.py
@@ -0,0 +1,9 @@
+
+alien_color = "blue"
+
+if alien_color == "green":
+    print("Player just earned 5 points")
+elif alien_color == "blue":
+    print("Player just earned 10 points")
+else:
+    print("Player earned just 0 points")
